e4bda777b39a02ecd8b3363931c256a58fd60049
Igor Minin <igorm6387@gmail.com>
2024-03-10 21:21:08 +0200

    Client and Server  Authroization drafts

--- client/app.py
+++ client/app.py
@@ -1,11 +1,65 @@
 #!/usr/bin/env python
-from flask import Flask
+from flask import Flask, request, session, abort, render_template, redirect
+from joserfc import jwt
+from joserfc.errors import BadSignatureError
 
 app = Flask(__name__)
+app.secret_key = 'secret'
 
-@app.route("/")
-def hello_world():
-    return "<p>Client</p>"
+jwt_secret = 'jwt secret'
+
+def validate_jwt(jwt):
+    try:
+        decoded = jwt.decode(jwt, jwt_secret)
+        return decoded.claims
+    except BadSignatureError: 
+        return {}
+
+def user_info(user_id):
+    return "<p>User id: {user_id}</p>".format(user_id)
+
+def check_auth(request):
+    """Check that user is authorized.
+
+    Returns:
+        User ID.
+    """
+    auth_header = request.headers.get('Authorization')
+
+    request_token = None
+    if auth_header and auth_header.startswith('Bearer '):
+        request_token = auth_header.split(' ')[1]
+
+    session_token = None
+    if 'token' in session:
+        session_token = session['token']
+
+    # Token from request takes priority over session
+    if request_token is not None:
+        claims = validate_jwt(request_token)
+        if 'user' in claims:
+            session['token'] = request_token
+            return claims['user']
+        else:
+            del session['b']
+            return None
+    
+    if session_token is not None:
+        claims = validate_jwt(session_token)
+        if 'user' in claims:
+            return claims['user']
+
+    return None
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    user_id = check_auth(request)
+    if user_id is None:
+        if request.method == 'GET':
+            return render_template('unauthorized.html')
+        else:
+            return redirect('http://localhost:8081/oauth/authorize')
+    return render_template('userinfo.html', user_id=user_id)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
--- server/app.py
+++ server/app.py
@@ -1,24 +1,5 @@
 #!/usr/bin/env python
-from flask import Flask
-from pymongo import MongoClient
-
-app = Flask(__name__)
-
-# TODO: read from .env
-client = MongoClient('mongodb', 27017)
-db = client['statistics']
-collection = db['counter']
-
-
-@app.route("/")
-def hello_world():
-    return "<p>Server. Invocation count: {count}</p>".format(count = stat())
-
-def stat():
-    # Use $inc to atomically increment the counter
-    result = collection.find_one_and_update({}, {'$inc': {'count': 1}}, upsert=True, return_document=True)
-
-    return result['count']
+from webapp.app import app
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
